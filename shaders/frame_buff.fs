d395d30aac07995b4fcbb75e797b82cf5927ef00
jathavan mahendrarajah <physickness@gmail.com>
2026-06-07 21:44:28 +0530

    post processing

--- shaders/frame_buff.fs
+++ shaders/frame_buff.fs
@@ -7,5 +7,8 @@ uniform sampler2D screenTexture;
 
 void main()
 { 
+    // FragColor = vec4(vec3(1.0f - texture(screenTexture, TexCoords)),1.0f); // negate
     FragColor = texture(screenTexture, TexCoords);
+    float average = (FragColor.r + FragColor.g + FragColor.b)/3.0f;
+    FragColor = vec4(average, average, average, 1.0f);
 }
